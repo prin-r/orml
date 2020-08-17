3b96b111bbe16fa6669fe745b63ed880a12936ed
prin-r <q@bandprotocol.com>
2020-08-17 10:07:53 +0700

    -

--- traits/src/lib.rs
+++ traits/src/lib.rs
@@ -1,10 +1,11 @@
 #![cfg_attr(not(feature = "std"), no_std)]
-
+use crate::Instance;
 use codec::{Decode, Encode};
 use sp_runtime::{DispatchResult, RuntimeDebug};
 use sp_std::{
 	cmp::{Eq, PartialEq},
 	prelude::Vec,
+	prelude::*,
 };
 
 pub use auction::{Auction, AuctionHandler, AuctionInfo, OnNewBidResult};
@@ -56,3 +57,56 @@ pub enum Change<Value> {
 	/// Changed to new value.
 	NewValue(Value),
 }
+
+/// A simple trait to provide data from a given ProviderId
+pub trait MultiDataProvider<ProviderId, Key, Value>: frame_system::Trait {
+	/// Provide a new value for given key and ProviderId from an operator
+	fn get(source: ProviderId, key: &Key) -> Option<Value>;
+}
+
+pub struct AggregatedDataProvider<T, I>;
+
+impl<T: Trait<I>, I: Instance> DataProvider<T::OracleKey, T::OracleValue> for AggregatedDataProvider<T, I> {
+	fn get(key: &T::OracleKey) -> Option<T::OracleValue> {
+		Self::get(key).map(|timestamped_value| timestamped_value.value)
+	}
+}
+
+// #[macro_export]
+// macro_rules! create_median_value_data_provider {
+// 	( TypeName:ident, $(Providers:ty),+ ) => {
+// 		pub trait Trait: system::Trait {
+// 			type Source1: DataProvider<CurrencyId, Price>;
+// 			type Source2: DataProvider<CurrencyId, Price>;
+// 			type Source3: DataProvider<CurrencyId, Price>;
+// 		}
+
+// 		impl $TypeName {
+// 			fn get(source: DataProviderId, key: &CurrencyId) -> Option<Price> {
+// 				match source {
+// 					DataProviderId::AcalaDataProvier => <AcalaOracle as DataProvider<CurrencyId, Price>>::get(&key),
+// 					DataProviderId::BandDataProvider => <BandOracle as DataProvider<CurrencyId, Price>>::get(&key),
+// 					DataProviderId::AggergatedDataProvider => None,
+// 				}
+// 			}
+
+// 			fn get(_key: &<T as Trait<I>>::OracleKey) -> Option<<T as Trait<I>>::OracleValue> {
+// 				let expires_in = ExpiresIn::get();
+// 				let now = T::Time::now();
+
+// 				values.retain(|x| x.timestamp + expires_in > now);
+
+// 				let count = values.len() as u32;
+// 				let minimum_count = MinimumCount::get();
+// 				if count < minimum_count {
+// 					return prev_value;
+// 				}
+
+// 				values.sort_by(|a, b| a.value.cmp(&b.value));
+
+// 				let median_index = count / 2;
+// 				Some(values[median_index as usize].clone())
+// 			}
+// 		}
+// 	};
+// }
